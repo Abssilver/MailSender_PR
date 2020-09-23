6fd78a6a9a31f60589f2336487db3259d630f29f
Павел Ремизов <remizov@rightway.games>
2020-09-23 23:43:23 +0300

    Тестовое-WPF приложение для отправки почты

--- Tests/WPFTests/MainWindow.xaml.cs
+++ Tests/WPFTests/MainWindow.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Windows;
 
-
-namespace ConsoleTests
+namespace WPFTests
 {
-    class Program
+    public partial class MainWindow 
     {
-        static void Main(string[] args)
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
         {
             var to = new MailAddress("test@gmail.com", "Test To");
             var from = new MailAddress("test@yandex.ru", "Test From");
@@ -17,19 +22,17 @@ namespace ConsoleTests
             message.Subject = "Заголовок письма от " + DateTime.Now;
             message.Body = "Текст тестового письма " + DateTime.Now;
 
-            var client = new SmtpClient("smtp.yandex.ru", 25);
+            var client = new SmtpClient("smtp.yandex.ru", 587);
 
             client.EnableSsl = true;
 
             client.Credentials = new NetworkCredential
             {
-                UserName = "user_name",
-                Password = "password"
+                UserName = LoginTb.Text,
+                SecurePassword = PasswordPb.SecurePassword
             };
 
             client.Send(message);
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
