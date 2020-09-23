4a5a5ab3dd5f85c7bc0ac8c167b1a9425c19c51b
Павел Ремизов <remizov@rightway.games>
2020-09-23 23:09:59 +0300

    Тестовый код отправки почты в консольном приложении

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+
 
 namespace ConsoleTests
 {
@@ -6,6 +9,24 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
+            var to = new MailAddress("test@gmail.com", "Test To");
+            var from = new MailAddress("test@yandex.ru", "Test From");
+
+            var message = new MailMessage(from, to);
+
+            message.Subject = "Заголовок письма от " + DateTime.Now;
+            message.Body = "Текст тестового письма " + DateTime.Now;
+
+            var client = new SmtpClient("smtp.yandex.ru", 587);
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user_name",
+                Password = "password"
+            };
+
+            client.Send(message);
+
             Console.WriteLine("Hello World!");
         }
     }
