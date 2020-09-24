61117d74ea4fec4214bf4eaa85796a6c6eba8eea
Павел Ремизов <remizov@rightway.games>
2020-09-24 23:56:49 +0300

    Выполнен пункт C

--- Tests/WPFTests/ConnectionData.cs
+++ Tests/WPFTests/ConnectionData.cs
@@ -1,12 +1,23 @@
-﻿namespace WPFTests
+﻿using System.Net.Mail;
+
+namespace WPFTests
 {
     static class ConnectionData
     {
-        public static string ClientEmail { get; } = "test@gmail.com";
+        public static string ClientEmail { get; } = "test@yandex.com";
         public static string AddresseeEmail { get; } = "test@gmail.com";
-        public static string SmtpServer { get; } = "smtp.yandex.ru";
-        public static int SmtpPort { get; } = 587;
+        public static string SmtpServerYandex { get; } = "smtp.yandex.ru";
+        public static int SmtpPortYandex { get; } = 587;
+        public static string SmtpServerGoogle { get; } = "smtp.gmail.com";
+        public static int SmtpPortGoogle { get; } = 58;
+        public static string SmtpServerMailRu { get; } = "smtp.mail.ru";
+        public static int SmtpPortMailRu{ get; } = 25;
         public static string AddresseeUsername { get; } = "Addressee";
         public static string ClientUsername { get; } = "Client";
+        public static bool EnableSSL { get; } = true;
+        public static bool IsBodyHtml { get; } = false;
+        public static bool UseDefaultCredentials { get; } = false;
+        public static SmtpDeliveryMethod DeliveryMethod { get; } = SmtpDeliveryMethod.Network;
+
     }
 }
---
+++ Tests/WPFTests/WpfMailSender.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace WPFTests
+{
+    public partial class WpfMailSender 
+    {
+        private EmailSendServiceClass _sendService;
+        public WpfMailSender()
+        {
+            InitializeComponent();
+            _sendService = new EmailSendServiceClass();
+        }
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            _sendService.SetupCredentials(LoginTb.Text, PasswordPb.SecurePassword);
+            _sendService.SetupMessage(EmailSubjectTb.Text, EmailBodyTb.Text);
+            try
+            {
+                _sendService.SendMessage();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessageWindow errorWindow = 
+                    new ErrorMessageWindow("Невозможно отправить письмо:\n" + ex.ToString());
+                errorWindow.ShowDialog();
+            }
+            SendEndWindow endWindow = new SendEndWindow();
+            endWindow.ShowDialog();
+        }
+    }
+}
