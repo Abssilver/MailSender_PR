115bb1d7409c40e4ec5c97b0e3dd9a7574e8bfc2
Павел Ремизов <remizov@rightway.games>
2020-09-29 00:44:53 +0300

    Работа с методичкой

--- MailSender/Data/TestData.cs
+++ MailSender/Data/TestData.cs
@@ -12,26 +12,56 @@ namespace MailSender.Data
         public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
             .Select(i => new Sender
             {
+                Id = i,
                 Name = $"Отправитель {i}",
-                Address = $"sender_{i}@server.ru"
+                Address = $"sender_{i}@server.ru",
+                Description = $"Почта от Отправитель {i}"
             }).ToList();
         public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 10)
             .Select(i => new Recipient
             {
+                Id =i,
                 Name = $"Получатель {i}",
-                Address = $"recipient_{i}@server.ru"
+                Address = $"recipient_{i}@server.ru",
+                Description = $"Почта для Получатель {i}"
             }).ToList();
-        public static List<Server> Servers { get; } = Enumerable.Range(1, 10)
-           .Select(i => new Server
-           {
-               Address = $"smtp.server_{i}@server.ru",
-               Login = $"Login-{i}",
-               Password = TextEncoder.Encode($"Password-{i}"),
-               UseSSL = i % 2 == 0
-           }).ToList();
+        public static List<Server> Servers { get; } = new List<Server>
+        {
+            new Server
+            {
+                Id = 1,
+                Name = "Яндекс",
+                Address = "smpt.yandex.ru",
+                Port = 587,
+                UseSSL = true,
+                Login = "user@yandex.ru",
+                Password = TextEncoder.Encode($"Password-Яндекс")
+            },
+            new Server
+            {
+                Id = 2,
+                Name = "Google",
+                Address = "smtp.gmail.com",
+                Port = 58,
+                UseSSL = true,
+                Login = "user@gmail.com",
+                Password = TextEncoder.Encode($"Password-Google")
+            },
+            new Server
+            {
+                Id = 3,
+                Name = "MailRu",
+                Address = "smtp.mail.ru",
+                Port = 25,
+                UseSSL = true,
+                Login = "user@mail.ru",
+                Password = TextEncoder.Encode($"Password-MailRu")
+            }
+        };
         public static List<Message> Messages { get; } = Enumerable.Range(1, 20)
            .Select(i => new Message
            {
+               Id = i,
                Subject = $"Сообщение {i}",
                Body = $"Текст сообщения {i}"
            }).ToList();
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using MailSender.lib;
 using MailSender.Models;
+using System.Diagnostics;
 using System.Net.Mail;
 using System.Windows;
+using System.Linq;
+using MailSender.Data;
 
 namespace MailSender
 {
@@ -11,6 +14,73 @@ namespace MailSender
         {
             InitializeComponent();
         }
+        /// <summary>Обработчик события кнопки создания нового сервера</summary>
+        private void OnAddServerButtonClick(object Sender, RoutedEventArgs E)
+        {
+            if (!ServerEditDialog.Create(
+            out var name,
+            out var address,
+            out var port,
+            out var ssl,
+            out var description,
+            out var login,
+            out var password))
+                return;
+            var server = new Server
+            {
+                Id = TestData.Servers.DefaultIfEmpty().Max(s => s.Id) + 1,
+                Name = name,
+                Address = address,
+                Port = port,
+                UseSSL = ssl,
+                Description = description,
+                Login = login,
+                Password = password
+            };
+            TestData.Servers.Add(server);
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+            ServersList.SelectedItem = server;
+        }
+
+        /// <summary>Обработчик события кнопки редактирования сервера</summary>
+        private void OnEditServerButtonClick(object Sender, RoutedEventArgs E)
+        {
+            if (!(ServersList.SelectedItem is Server server)) return;
+            var name = server.Name;
+            var address = server.Address;
+            var port = server.Port;
+            var ssl = server.UseSSL;
+            var description = server.Description;
+            var login = server.Login;
+            var password = server.Password;
+            if (!ServerEditDialog.ShowDialog("Редактирование сервера",
+            ref name,
+            ref address, ref port, ref ssl,
+            ref description,
+            ref login, ref password))
+                return;
+            server.Name = name;
+            server.Address = address;
+            server.Port = port;
+            server.UseSSL = ssl;
+            server.Description = description;
+            server.Login = login;
+            server.Password = password;
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+        }
+
+        /// <summary>Обработчик события кнопки удаления сервера</summary>
+        private void OnDeleteServerButtonClick(object Sender, RoutedEventArgs E)
+        {
+            if (!(ServersList.SelectedItem is Server server)) return;
+            TestData.Servers.Remove(server);
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+            ServersList.SelectedItem = TestData.Servers.FirstOrDefault();
+        }
+        /// <summary>Обработчик события кнопки немедленной отправки поты</summary>
         private void OnSendButtonClick(object sender, RoutedEventArgs args)
         {
             if (!(SendersList.SelectedItem is Sender selectedSender)) return;
@@ -18,6 +88,33 @@ namespace MailSender
             if (!(ServersList.SelectedItem is Server selectedServer)) return;
             if (!(MessagesList.SelectedItem is Message selectedMessage)) return;
 
+            var mailSender = new SmtpSender(
+            selectedServer.Address, selectedServer.Port, selectedServer.UseSSL,
+            selectedServer.Login, selectedServer.Password);
+
+            try
+            {
+                var timer = Stopwatch.StartNew();
+                mailSender.SendMessage(
+                selectedSender.Address, selectedRecipient.Address,
+                selectedMessage.Subject, selectedMessage.Body);
+                timer.Stop();
+                MessageBox.Show(
+                $"Почта успешно отправлена за {timer.Elapsed.TotalSeconds:0.##}c",
+                "Отправка почты",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+            }
+            catch (SmtpException)
+            {
+                MessageBox.Show(
+                "Ошибка при отправке почты",
+                "Отправка почты",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            }
+            
+            /*
             var sendService = new MailSenderService()
             {
                 ServerAddress = selectedServer.Address,
@@ -37,6 +134,7 @@ namespace MailSender
                 MessageBox.Show("Ошибка при отправке почты\n" + ex.ToString(), "Ошибка", 
                     MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            */
         }
     }
 }
