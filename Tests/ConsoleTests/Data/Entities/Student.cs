e13066673adf0375a62004b89060d7a1f867c695
Павел Ремизов <remizov@rightway.games>
2020-10-14 23:53:42 +0300

    Урок-7 часть 2

---
+++ Tests/ConsoleTests/Data/Entities/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConsoleTests.Data.Entities
+{
+    abstract class Entity
+    {
+        //[Key]
+        public int Id { get; set; }
+
+    }
+    abstract class NamedEntity : Entity
+    {
+        public string Name { get; set; }
+    }
+    class Student: NamedEntity
+    {
+        [Required, MaxLength(50)]
+        public string Surname { get; set; }
+        [MaxLength(50)]
+        public string Patronymic { get; set; }
+
+        public virtual Group Group { get; set; } //virtual - тут навигационное свойство
+
+    }
+    class Group: NamedEntity
+    {
+        public string Description { get; set; }
+
+        public virtual ICollection<Student> strudents { get; set; } //отложенные запросы к даннымм
+    }
+
+}
