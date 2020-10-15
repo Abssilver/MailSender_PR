5aa1af4ed8e9f9c43f6da8a817143c9d81500af1
Павел Ремизов <remizov@rightway.games>
2020-10-16 01:02:17 +0300

    Урок-7 часть 4

---
+++ MailSender.lib/Models/SchedulerTask.cs
@@ -0,0 +1,16 @@
+﻿using MailSender.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    public class SchedulerTask : Entity
+    {
+        public DateTime Time { get; set; }
+        public Server Server { get; set; }
+        public Sender Sender { get; set; }
+        public ICollection<Recipient> Recipients { get; set; }
+        public Message Message { get; set; }
+    }
+}
---
+++ MailSender/Data/MailSenderDbContextInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MailSender.Data
+{
+    class MailSenderDbContextInitializer : IDesignTimeDbContextFactory<MailSenderDB>
+    {
+        public MailSenderDB CreateDbContext(string[] args)
+        {
+            const string connection =
+                @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MailSender.DB;Integrated Security=True";
+
+            var optionsBuilder = new DbContextOptionsBuilder<MailSenderDB>();
+            optionsBuilder.UseSqlServer(connection);
+
+            return new MailSenderDB(optionsBuilder.Options);
+        }
+    }
+}
