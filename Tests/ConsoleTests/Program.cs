f5fd346b27c7ee361d92e4559772bbc4b550ada0
Павел Ремизов <remizov@rightway.games>
2020-10-20 23:19:06 +0300

    Урок 8 часть 4

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -8,9 +8,11 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,13 +21,43 @@ namespace ConsoleTests
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var report = new StatisticReport();
+            Assembly assembly = Assembly.GetEntryAssembly();
 
-            report.SendedMessagesCount = 1000;
-            report.CreatePackage("statistics.docx");
+            Type type = assembly.GetType("ConsoleTests.Program");
+            Type type2 = assembly.GetTypes().First(type => type.Name.Equals("Program"));
+
+            var str = "test";
+
+            Type type3 = GetObjectType(str);
+            var typeString = typeof(string);
+
+            var fileInfo = new FileInfo("TestLib.dll");
+            var testLib = Assembly.LoadFile(fileInfo.FullName);
+            var printerType = testLib.GetType("TestLib.Printer");
+            //constructorInfo
+            //MethodInfo
+            //ParameterInfo
+            //PropertyInfo
+            //EventInfo
+            //FieldInfo
+            foreach (var method in printerType.GetMethods())
+            {
+                var returnType = method.ReturnType;
+                var parameters = method.GetParameters();
+
+                Console.WriteLine
+                    ($"{returnType.Name} {method.Name}({string.Join(",", parameters.Select(p=>$"{p.ParameterType.Name} {p.Name}"))})");
+            }
+            object printer = Activator.CreateInstance(printerType, ">>>");
+
+            var printerConstructor = printerType.GetConstructor(new[] { typeof(string) });
             Console.ReadLine();
+        }
+        private static Type GetObjectType(object obj)
+        {
+            return obj.GetType();
         }
     }
 }
---
+++ Tests/TestLib/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLib
+{
+    public class Printer
+    {
+        private string _prefix;
+        public Printer(string prefix)
+        {
+            _prefix = prefix;
+        }
+        public virtual void Print(string message)
+        {
+            Console.WriteLine($"{_prefix}{message}");
+        }
+    }
+    internal class InternalPrinter : Printer
+    {
+        public int Value { get; set; }
+        public InternalPrinter(): base("Internal")
+        {
+        }
+        public virtual void Print(string message)
+        {
+            Console.WriteLine($"Internal {Value}");
+            base.Print(message);
+        }
+    }
+}
